2a193530ab6e553248abe903fe02cd55214bbed7
Martin Naumann <martin.naumann@centralway.com>
2013-10-14 20:14:23 +0200

    Auth ALL THE CONTROLLERS

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,29 @@
-function PostListCtrl($scope, Posts) {
+function PostListCtrl($scope, $routeParams, angularFireAuth, firebaseRef, Posts) {
+  angularFireAuth.initialize(firebaseRef, {scope: $scope, name: "user"});
+    
+  $scope.login = function() {
+    angularFireAuth.login("twitter");
+  };
+    
+  $scope.logout = function() {
+    angularFireAuth.logout();
+  };
+    
   $scope.posts = Posts;
+  $scope.author = $routeParams.author;
 }
 
-function PostWriteCtrl($scope, $location, Posts) {
-  $scope.post = {title: 'Add title', text: '##Add some content'};
+function PostWriteCtrl($scope, $location, angularFireAuth, firebaseRef, Posts) {
+  angularFireAuth.initialize(firebaseRef, {scope: $scope, name: "user"});
+    
+  $scope.post = { title: 'Add title', text: '##Add some content' };
   $scope.publish = function() {
-    Posts.push($scope.post);
-    $location.path('/');
+    if(!$scope.user) return;
+      
+    $scope.post.author = $scope.user.id;      
+    console.log($scope.user, $scope.post.author);      
+
+    Posts.add($scope.post);
+    //$location.path('/');
   };
 }
