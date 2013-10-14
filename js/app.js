ed66c6f3f0e868f339078d4f95c47d15ac6644cd
Martin Naumann <martin.naumann@centralway.com>
2013-10-14 20:14:00 +0200

    Reworked firebase and routes

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var writedown = angular.module('writedown',['firebase', 'angular-markdown'])
-  .value('firebaseURL', 'https://blogdown.firebaseio.com/')
+  .value('firebaseRef', new Firebase('https://blogdown.firebaseio.com/posts'))
   .config(['$routeProvider', '$locationProvider', '$httpProvider', function($routeProvider, $locationProvider, $httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
     
-    $routeProvider.when('/', {controller: PostListCtrl, templateUrl: 'views/home.html'});
     $routeProvider.when('/write', {controller: PostWriteCtrl, templateUrl: 'views/write.html'});
-    $routeProvider.otherwise({redirectTo: '/'});
+    $routeProvider.when('/blog/:author', {controller: PostListCtrl, templateUrl: 'views/home.html'});
+    $routeProvider.otherwise({redirectTo: '/blog/'});
     $locationProvider.html5Mode(false);
   }]);  
